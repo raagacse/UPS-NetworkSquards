4eb7c9b88c8a89c1293fc7eba48c4d931db1df76
Rajesh S <47102382+raagacse@users.noreply.github.com>
2024-07-16 19:19:58 +0530

    model

---
+++ Distance.cs
@@ -0,0 +1,10 @@
+
+namespace DistanceFinder;
+
+public class ClsDistance
+{
+    public string StartAddress { get; set; }
+    public string EndAddress { get; set; }
+    public string  Distance { get; set; }
+    public string Duration { get; set; }
+}
